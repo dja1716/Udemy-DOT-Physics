159c89296e7c1d3627cbac38cc5c760505c93373
dja1716 <hdum@uwaterloo.ca>
2021-03-24 14:02:40 -0400

    first

---
+++ Assets/DOTS_Physics/BallJumpSystem.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Physics;
+
+public class BallJumpSystem : ComponentSystem {
+    
+    protected override void OnUpdate() {
+        Entities.ForEach((ref PhysicsVelocity physicsVelocity) => {
+            if (Input.GetKeyDown(KeyCode.Space)) {
+                physicsVelocity.Linear.y = 5f;
+            }
+        });
+    }
+
+}
---
+++ Assets/DOTS_Physics/TestTriggers.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Physics;
+using Unity.Physics.Systems;
+using Unity.Mathematics;
+using Unity.Burst;
+
+public class TestTriggers : JobComponentSystem {
+
+    [BurstCompile]
+    private struct TriggerJob : ITriggerEventsJob {
+
+        public ComponentDataFromEntity<PhysicsVelocity> physicsVelocityEntities;
+
+        public void Execute(TriggerEvent triggerEvent) {
+            if (physicsVelocityEntities.HasComponent(triggerEvent.Entities.EntityA)) {
+                PhysicsVelocity physicsVelocity = physicsVelocityEntities[triggerEvent.Entities.EntityA];
+                physicsVelocity.Linear.y = 5f;
+                physicsVelocityEntities[triggerEvent.Entities.EntityA] = physicsVelocity;
+            }
+            
+            if (physicsVelocityEntities.HasComponent(triggerEvent.Entities.EntityB)) {
+                PhysicsVelocity physicsVelocity = physicsVelocityEntities[triggerEvent.Entities.EntityB];
+                physicsVelocity.Linear.y = 5f;
+                physicsVelocityEntities[triggerEvent.Entities.EntityB] = physicsVelocity;
+            }
+        }
+
+    }
+
+
+    private BuildPhysicsWorld buildPhysicsWorld;
+    private StepPhysicsWorld stepPhysicsWorld;
+
+    protected override void OnCreate() {
+        buildPhysicsWorld = World.GetOrCreateSystem<BuildPhysicsWorld>();
+        stepPhysicsWorld = World.GetOrCreateSystem<StepPhysicsWorld>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps) {
+        TriggerJob triggerJob = new TriggerJob {
+             physicsVelocityEntities = GetComponentDataFromEntity<PhysicsVelocity>()
+        };
+        return triggerJob.Schedule(stepPhysicsWorld.Simulation, ref buildPhysicsWorld.PhysicsWorld, inputDeps);
+    }
+
+}
